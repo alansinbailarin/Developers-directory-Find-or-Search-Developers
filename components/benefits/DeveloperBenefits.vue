2484aae7953205f84d31699cea0392903215d0d2
Admin <admin@AlanPacheco.local>
2024-04-03 22:58:01 -0700

    changed images

--- components/benefits/DeveloperBenefits.vue
+++ components/benefits/DeveloperBenefits.vue
@@ -19,7 +19,7 @@
       </div>
       <div></div>
       <div
-        class="bg-white shadow-md p-4 text-center self-end col-span-2 rounded-lg"
+        class="bg-white hidden md:block shadow-md p-4 text-center self-end col-span-2 rounded-lg"
       >
         <picture>
           <img
@@ -43,7 +43,7 @@
         </div>
       </div>
       <div
-        class="bg-white shadow-md p-4 text-center self-start md:mb-20 col-span-2 rounded-lg"
+        class="bg-white shadow-md p-4 hidden md:block text-center self-start md:mb-20 col-span-2 rounded-lg"
       >
         <picture>
           <img
@@ -68,7 +68,7 @@
         </div>
       </div>
       <div
-        class="bg-white shadow-md p-4 text-center self-end md:mt-20 col-span-2 rounded-lg"
+        class="bg-white shadow-md p-4 hidden md:block text-center self-end md:mt-20 col-span-2 rounded-lg"
       >
         <picture>
           <img
@@ -92,7 +92,7 @@
         </div>
       </div>
       <div
-        class="bg-white shadow-md p-4 text-center self-start md:mb-20 col-span-2 rounded-lg"
+        class="bg-white shadow-md p-4 hidden md:block text-center self-start md:mb-20 col-span-2 rounded-lg"
       >
         <picture>
           <img
@@ -113,6 +113,104 @@
             There are several alternatives to find your desired job, whether it
             is a freelance project or a regular job. Once your profile is
             completed, you just have to wait or apply.
+          </p>
+        </div>
+      </div>
+      <div
+        class="bg-white shadow-md p-4 block md:hidden text-center self-start md:mb-20 col-span-2 rounded-lg"
+      >
+        <picture>
+          <img
+            src="../../assets/images/profile.jpg"
+            alt="Profile"
+            class="w-full h-full rounded object-cover mb-5"
+          />
+        </picture>
+        <div class="text-left">
+          <span
+            class="bg-gray-100 px-3 py-2 rounded-md text-gray-600 font-medium text-sm"
+            >Validate yourself</span
+          >
+          <h1 class="text-gray-900 font-medium text-2xl mt-3">
+            Start by creating your profile
+          </h1>
+          <p class="text-lg text-gray-600 mt-2">
+            Complete the information in your profile, your information will come
+            to us directly, we will validate it to ensure that it is real
+            information.
+          </p>
+        </div>
+      </div>
+      <div
+        class="bg-white block md:hidden shadow-md p-4 text-center self-end col-span-2 rounded-lg"
+      >
+        <picture>
+          <img
+            src="../../assets/images/validated.jpg"
+            alt="Profile validation"
+            class="w-full h-full mb-5 rounded object-cover"
+          />
+        </picture>
+        <div class="text-left">
+          <span
+            class="bg-gray-100 px-3 py-2 rounded-md text-gray-600 font-medium text-sm"
+            >Validate by us</span
+          >
+          <h1 class="text-gray-900 font-medium text-2xl mt-3">
+            We will validate you
+          </h1>
+          <p class="text-lg text-gray-600 mt-2">
+            Once your profile is complete, the devsdir team will review your
+            professional profile, send you feedback or approve your profile.
+          </p>
+        </div>
+      </div>
+      <div
+        class="bg-white shadow-md p-4 block md:hidden text-center self-start md:mb-20 col-span-2 rounded-lg"
+      >
+        <picture>
+          <img
+            src="../../assets/images/search_job.jpg"
+            alt="Search job"
+            class="w-full h-full rounded object-cover mb-5"
+          />
+        </picture>
+        <div class="text-left">
+          <span
+            class="bg-gray-100 px-3 py-2 rounded-md text-gray-600 font-medium text-sm"
+            >Search or wait</span
+          >
+          <h1 class="text-gray-900 font-medium text-2xl mt-3">
+            Wait for an offer or search for a project
+          </h1>
+          <p class="text-lg text-gray-600 mt-2">
+            There are several alternatives to find your desired job, whether it
+            is a freelance project or a regular job. Once your profile is
+            completed, you just have to wait or apply.
+          </p>
+        </div>
+      </div>
+      <div
+        class="bg-white shadow-md p-4 block md:hidden text-center self-end md:mt-20 col-span-2 rounded-lg"
+      >
+        <picture>
+          <img
+            src="../../assets/images/development.jpeg"
+            alt="Development"
+            class="w-full h-full mb-5 rounded object-cover"
+          />
+        </picture>
+        <div class="text-left">
+          <span
+            class="bg-gray-100 px-3 py-2 rounded-md text-gray-600 font-medium text-sm"
+            >Get hired</span
+          >
+          <h1 class="text-gray-900 font-medium text-2xl mt-3">
+            Start with your job
+          </h1>
+          <p class="text-lg text-gray-600 mt-2">
+            Once your profile is validated, you have job offers or freelance
+            projects, get hired and start enjoying the job of your dreams.
           </p>
         </div>
       </div>
